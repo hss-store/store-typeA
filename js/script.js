dca4118a022e1aff9ce390156e1663903e599ed7
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-09-15 16:13:09 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function test() {
+    var retext = datebaseRequest("https://script.google.com/macros/s/AKfycbwUdfIf495kezAIQxtht5WMjjIAqOPe1YAC2x6nE0me_t4eRz27HCv-x8Hc10PZvppL/exec",["type=auth","userName=unko","passHash=aiueo700"]);
+    var start = retext.indexOf("A HREF") + 7;
+    var end = retext.indexOf(">here<") - 2;
+    retext = retext.substring(start,end);
+    console.log(datebaseRequest(retext));
 }
 
 function datebaseRequest(url, params) {
@@ -16,8 +21,9 @@ function datebaseRequest(url, params) {
 
     xmlRequest.onreadystatechange = function() {
         if(xmlRequest.readyState === 4 && xmlRequest.status === 200) {
-           console.log(xmlRequest.responseText);
-           return xmlRequest.responseText;
+            var retext = xmlRequest.responseText;
+            console.log(retext);
+            return retext;
         }
     }
 }
